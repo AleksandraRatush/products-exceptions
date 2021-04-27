855edd92160a865e45162de1ec000a5190b815e0
Ratush Aleksandra <Ne0f1ex>
2021-04-27 22:37:15 +0400

    fix remark

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -11,46 +11,41 @@ import ru.netology.domain.exception.NotFoundException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
-  private ProductRepository repository = new ProductRepository();
-  private Book coreJava = new Book();
-  private TShirt tshirt = new TShirt();
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book();
+    private TShirt tshirt = new TShirt();
 
-  @BeforeEach
-  public void clear(){
-     repository = new ProductRepository();
-  }
-
-  @Test
-  public void shouldSaveOneItem() {
-    repository.save(coreJava);
-    Product[] expected = new Product[]{coreJava};
-    Product[] actual = repository.findAll();
-    assertArrayEquals(expected, actual);
-  }
-
-  @Test
-  public void shouldRemoveOne() {
-    repository.save(coreJava);
-    repository.save(tshirt);
-    repository.removeById(1);
-    Product[] actual = repository.findAll();
-    Product[] expected = new Product[]{tshirt};
-    assertArrayEquals(expected, actual);
-  }
-
-  @Test
-  public void shouldFailOnRemoveNotFound() {
-    repository.save(coreJava);
-    repository.save(tshirt);
-    try {
-      repository.removeById(3);
-    } catch (Throwable ex) {
-       assertTrue(ex instanceof NotFoundException);
-       assertEquals(new NotFoundException("Element with id: 3 not found").getMessage()
-               , ex.getMessage());
+    @BeforeEach
+    public void clear() {
+        repository = new ProductRepository();
     }
-  }
 
+    @Test
+    public void shouldSaveOneItem() {
+        repository.save(coreJava);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveOne() {
+        repository.save(coreJava);
+        repository.save(tshirt);
+        repository.removeById(1);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{tshirt};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFailOnRemoveNotFound() {
+        repository.save(coreJava);
+        repository.save(tshirt);
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(3);
+        });
+    }
 
 
 }
